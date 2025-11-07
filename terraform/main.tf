36186a41cd4b7d8abaf6396bec465ccd22d38a8c
User <ryan.sissom@yahoo.com>
2025-11-06 21:34:00 -0600

    Provision EKS cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,6 +64,35 @@ module "vpc" {
   # }
 }
 
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "my-cluster"
+  kubernetes_version = "1.33"
+  
+  # Networking
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
+
+  # Managed node group
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t2.micro"]
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 3
+
+      # Optional extras
+      disk_size      = 20
+      capacity_type  = "SPOT"
+    }
+  }
+}
+
 ########################################
 # Resources
 ########################################
